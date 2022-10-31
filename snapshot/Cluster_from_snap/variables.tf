3ba40b9aa1371afd1f78b3a71bfd8517b6d56138
DeepakshiPurushotham <107757971+DeepakshiPurushotham@users.noreply.github.com>
2022-10-31 11:00:43 +0530

    Add files via upload

---
+++ snapshot/Cluster_from_snap/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  default = "us-east-1"
+}
+variable "access_key" {
+  type = string
+  #default = "AKIAZ5VTOSHXNUYDJ676"\ deepa
+  #default = "AKIA56J2SXVB4TGQZEXB"
+}
+
+variable "secret_key" {
+  type = string
+  #default = "Lg50mFo5p7l6mGeUs1VCujfaEK4TXqeDeLozN1YB" \deepa
+  #default = "wk4mMKlMATB+Gro+QwNw1TbE0jhwdQ5sNGT/fFiC"
+}
