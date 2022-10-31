7ace679a81fd2e0240ed89df93e44dcaaba78d52
DeepakshiPurushotham <107757971+DeepakshiPurushotham@users.noreply.github.com>
2022-10-31 11:12:48 +0530

    Update variables.tf

--- snapshot/variables.tf
+++ snapshot/variables.tf
@@ -1,12 +1,3 @@
 variable "region" {
   default = "us-east-1"
 }
-variable "access_key" {
-  type = string
-  #default = "AKIAZ5VTOSHXNUYDJ676"
-}
-
-variable "secret_key" {
-  type = string
-  #default = "Lg50mFo5p7l6mGeUs1VCujfaEK4TXqeDeLozN1YB"
-}
