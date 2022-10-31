3edeb29e5f71a507547c9e4f507657fb22700864
DeepakshiPurushotham <107757971+DeepakshiPurushotham@users.noreply.github.com>
2022-10-31 12:23:49 +0530

    Update main.tf

--- snapshot/main.tf
+++ snapshot/main.tf
@@ -10,8 +10,6 @@ terraform {
 provider "aws" {
   # Configuration options
   region = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
 }
 data "aws_rds_cluster" "aurora-cluster-demo" {
   cluster_identifier = "aurora-cluster-demo"
