44ccc20bc3d0d8856b912aefa6246ab5c98bc8dc
DeepakshiPurushotham <107757971+DeepakshiPurushotham@users.noreply.github.com>
2022-10-31 11:03:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,10 +3,10 @@ variable "region" {
 }
 variable "access_key" {
   type = string
-  #default = "AKIAZ5VTOSHXNUYDJ676"
+ 
 }
 
 variable "secret_key" {
   type = string
-  #default = "Lg50mFo5p7l6mGeUs1VCujfaEK4TXqeDeLozN1YB"
+  
 }
